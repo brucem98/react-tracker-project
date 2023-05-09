d63977d70ca5a3c392095044dd9e0540f84e10c0
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-05-09 22:04:17 +0100

    Create Task component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,9 +1,11 @@
-export const Tasks = ({ tasks }) => {
-    const [tasks, setTasks] = useState([]);
+import { Task } from "./Task";
 
+export const Tasks = ({ tasks }) => {
     return (
         <>
-            {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
+            {tasks.map((task) => (
+                <Task key={task.id} task={task} />
+            ))}
         </>
     )
 }
