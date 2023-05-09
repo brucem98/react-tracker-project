bb3d63ec8ac7f53c662f58bbcea2720185b84b7f
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-05-09 21:54:14 +0100

    create tasks component

---
+++ src/components/Tasks.js
@@ -0,0 +1,32 @@
+export const Tasks = ({ tasks }) => {
+    const [tasks, setTasks] = useState([]);
+
+    return (
+        <>
+            {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
+        </>
+    )
+}
+
+
+// DUMMY DATA
+// const tasks = [
+//         {
+//             id: 1,
+//             text: 'Doctors Appointment',
+//             day: 'Feb 5th at 2:30pm',
+//             reminder: true,
+//         },
+//         {
+//             id: 2,
+//             text: 'Meeting at School',
+//             day:'Feb 6th at 1:30pm',
+//             reminder: true,
+//         },
+//         {
+//             id: 3,
+//             text: 'Food Shopping',
+//             day:'Feb 5th at 2:30pm',
+//             reminder: false,
+//         }
+//     ]
