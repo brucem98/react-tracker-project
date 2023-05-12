23ac62e1b07c25b863c0467b20761a57c6b4505d
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-05-12 15:47:38 +0100

    Initial button toggle setup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Tasks } from './components/Tasks';
 import { AddTask } from './components/AddTask';
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
       {
           id: 1,
@@ -23,7 +24,7 @@ const App = () => {
           day:'Feb 5th at 2:30pm',
           reminder: false,
       }
-  ])
+  ]);
   // Add Task
   const addTask = (task) => {
     //Since we dont have a backend to create an id on its own
@@ -46,8 +47,8 @@ const App = () => {
 
   return (
     <div className='container'>
-      <Header />
-      <AddTask onAdd={addTask}/>
+      <Header onAdd={() => setShowAddTask(!showAddTask)}/>
+      {showAddTask && <AddTask onAdd={addTask}/>} 
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}  onToggle={toggleReminder}/> : 'No Tasks Available'}
     </div>
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import PropTypes from 'prop-types';
 import { Button } from './Button';
 
-export const Header = ({ title }) => {
-  const onClick = () => {
-    console.log('Click');
-  }
+export const Header = ({ title, onAdd }) => {
+  // const onClick = () => {
+  //   console.log('Click');
+  // }
 
   return (
     <header className='header'>
         <h1>{title}</h1>
-        <Button color='green' text='Add' onClick={onClick}/>
+        <Button color='green' text='Add' onClick={onAdd}/>
     </header>
   )
 }
