6bd8aa188274869593dfc0c2144602c0dfb6c12f
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-05-12 15:39:38 +0100

    Add task submit

--- src/App.js
+++ src/App.js
@@ -24,6 +24,13 @@ const App = () => {
           reminder: false,
       }
   ])
+  // Add Task
+  const addTask = (task) => {
+    //Since we dont have a backend to create an id on its own
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  }
 
   // Delete Task
   const deleteTask = (id) => {
@@ -40,7 +47,7 @@ const App = () => {
   return (
     <div className='container'>
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}  onToggle={toggleReminder}/> : 'No Tasks Available'}
     </div>
   )
