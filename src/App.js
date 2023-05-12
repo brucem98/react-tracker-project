37690f373f2c66b4403ed1dad31802efeabfd67b
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-05-12 15:03:39 +0100

    Create AddTask component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Header } from './components/Header';
 import { Tasks } from './components/Tasks';
+import { AddTask } from './components/AddTask';
 
 const App = () => {
   const [tasks, setTasks] = useState([
@@ -38,7 +39,8 @@ const App = () => {
 
   return (
     <div className='container'>
-      <Header/>
+      <Header />
+      <AddTask />
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}  onToggle={toggleReminder}/> : 'No Tasks Available'}
     </div>
   )
