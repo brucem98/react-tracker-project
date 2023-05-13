58bfb363011cdab270401251953fe33e09bfaacc
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-05-13 10:56:01 +0100

    useLocation on Add Button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,21 @@
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
 import { Button } from './Button';
 
 export const Header = ({ title, onAdd, showAdd }) => {
   // const onClick = () => {
   //   console.log('Click');
   // }
+  const location = useLocation()
 
   return (
     <header className='header'>
         <h1>{title}</h1>
-        <Button color={showAdd ? 'Red' : 'Green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
+        {location.pathname === '/' && <Button 
+          color={showAdd ? 'Red' : 'Green'} 
+          text={showAdd ? 'Close' : 'Add'} 
+          onClick={onAdd}
+        />}
     </header>
   )
 }
